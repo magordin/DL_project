8cfa0e80e1ab7494bfc2a93cea77bfeccbf493fa
magordin <marianna.gordin@gmail.com>
2026-05-14 17:30:11 +0200

    path again

--- 03b_run_bulkformer.sh
+++ 03b_run_bulkformer.sh
@@ -13,8 +13,8 @@ module load cuda/12.4
 source /work3/s252608/DL_project/.venv/bin/activate
 
 export PROJECT_ROOT="/work3/s252608/DL_project"
-export BULKFORMER_DIR="$PROJECT_ROOT/BulkFormer"
-export PYTHONPATH="$PROJECT_ROOT:$BULKFORMER_DIR:$PYTHONPATH"
+export BULKFORMER_BASE="$PROJECT_ROOT/BulkFormer"
+export PYTHONPATH="$PROJECT_ROOT:$BULKFORMER_BASE:$PYTHONPATH"
 cd $PROJECT_ROOT
 
 python scripts/02_build_representations.py \
